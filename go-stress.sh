e455399436ab76e5dd9883ad2601f08265e8bbf0
davidescus <rahthman.s@gmail.com>
2019-12-30 11:04:50 +0200

    Add go-stress tool

---
+++ go-stress.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+# go test -c
+# comment above and uncomment below to enable the race builder
+go test -c -race
+PKG=$(basename $(pwd))
+
+while true ; do
+        export GOMAXPROCS=$[ 1 + $[ RANDOM % 128 ]]
+        ./${PKG}.test $@ 2>&1
+done
